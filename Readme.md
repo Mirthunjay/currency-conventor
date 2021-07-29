d84cbef36d744211cc15e25c897a637e8389aae5
Mirthunjay <83975266+Mirthunjay@users.noreply.github.com>
2021-07-29 22:22:23 +0530

    Added My files

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## Currency Conventor
+
+The Project I made That will convert the currency of one country and other country. I build the conventor using the Frankfurter API 
+
+To View my work
---
+++ script.js
@@ -0,0 +1,48 @@
+const select = document.querySelectorAll("#field-select");
+const input = document.querySelector("#field1-input");
+const output = document.querySelector("#field2-input");
+
+fetch("https://api.frankfurter.app/currencies", {
+  method: "GET",
+})
+  .then((data) => data.json())
+  .then((data) => loadCountryName(data));
+
+// loading the country Names
+function loadCountryName(data) {
+  const currencyName = Object.entries(data);
+
+  for (let i = 0; i < currencyName.length; i++) {
+    select[0].innerHTML += `<option value="${currencyName[i][0]}">${currencyName[i][0]} : ${currencyName[i][1]}</option>`;
+    select[1].innerHTML += `<option value="${currencyName[i][0]}">${currencyName[i][0]} : ${currencyName[i][1]}</option>`;
+  }
+}
+
+//Update the value when we Select and If user choose the same currency to aleart them to change
+
+function updateValue() {
+  let currency1 = select[0].value;
+  let currency2 = select[1].value;
+
+  let inputValue = input.value;
+
+  if (currency1 != currency2) {
+    convertCurrency(currency1, currency2, inputValue);
+  } else if (inputValue <= 0) {
+    alert("Please Enter Correct value !!!");
+  } else {
+    alert("Please choose different Currency !!!");
+  }
+}
+
+// Converting the Currency
+function convertCurrency(currency1, currency2, inputValue) {
+  fetch(
+    `https://api.frankfurter.app/latest?amount=${inputValue}&from=${currency1}&to=${currency2}`
+  )
+    .then((data) => data.json())
+    .then((data) => {
+      console.log(Object.values(data.rates)[0]);
+      output.value = Object.values(data.rates)[0];
+    });
+}
